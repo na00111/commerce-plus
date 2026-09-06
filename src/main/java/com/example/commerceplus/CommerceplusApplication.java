9c66b282d824de23a7a150a85a32b07701b2f91a
wonyeolShin <lwy171400@gmail.com>
2026-09-06 13:40:56 +0900

    first commit

---
+++ src/main/java/com/example/commerceplus/CommerceplusApplication.java
@@ -0,0 +1,13 @@
+package com.example.commerceplus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommerceplusApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CommerceplusApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/commerceplus/CommerceplusApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.commerceplus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommerceplusApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
