85df55ace371d54c3cd715acb8fd00d20a1e8763
wonyeolShin <lwy171400@gmail.com>
2026-09-06 16:57:18 +0900

    [feat/member] member 엔티티 수정

--- src/main/java/com/example/commerceplus/domain/member/repository/MemberRepository.java
+++ src/main/java/com/example/commerceplus/domain/member/repository/MemberRepository.java
@@ -1,9 +1,10 @@
 package com.example.commerceplus.domain.member.repository;
 
+import com.example.commerceplus.domain.member.entity.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.lang.reflect.Member;
+
 
 @Repository
 public interface MemberRepository extends JpaRepository<Member, Long> {
