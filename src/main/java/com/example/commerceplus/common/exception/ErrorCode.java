bc28e7561f0057dc129a6b4a389f83d3e57e7f1a
wonyeolShin <lwy171400@gmail.com>
2026-09-08 12:40:44 +0900

    [fix/common] errorCode 추가 (패스워드와 검증패스워드 불일치)

--- src/main/java/com/example/commerceplus/common/exception/ErrorCode.java
+++ src/main/java/com/example/commerceplus/common/exception/ErrorCode.java
@@ -16,6 +16,7 @@ public enum ErrorCode {
     MEMBER_NOT_FOUND(HttpStatus.NOT_FOUND, "MEMBER_001", "회원을 찾을 수 없습니다."),
     DUPLICATE_EMAIL(HttpStatus.CONFLICT, "MEMBER_002", "이미 존재하는 이메일입니다."),
     INVALID_CREDENTIALS(HttpStatus.UNAUTHORIZED, "MEMBER_003", "이메일 또는 비밀번호가 올바르지 않습니다."),
+    PASSWORD_CONFIRMATION_MISMATCH(HttpStatus.BAD_REQUEST, "MEMBER_004", "비밀번호와 확인 비밀번호가 일치하지 않습니다."),
 
     // Product
     PRODUCT_NOT_FOUND(HttpStatus.NOT_FOUND, "PRODUCT_001", "상품을 찾을 수 없습니다."),
