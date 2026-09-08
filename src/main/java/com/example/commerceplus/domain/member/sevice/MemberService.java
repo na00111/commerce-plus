950f9b91e753295c50a8be86b514e5876e296296
wonyeolShin <lwy171400@gmail.com>
2026-09-08 12:55:09 +0900

    [fix/common] 회원가입 메서드 서비스 추가

---
+++ src/main/java/com/example/commerceplus/domain/member/sevice/MemberService.java
@@ -0,0 +1,43 @@
+package com.example.commerceplus.domain.member.sevice;
+
+import com.example.commerceplus.common.bean.PasswordEncoder;
+import com.example.commerceplus.common.exception.BusinessException;
+import com.example.commerceplus.common.exception.ErrorCode;
+import com.example.commerceplus.domain.member.dto.request.CreateMemberRequest;
+import com.example.commerceplus.domain.member.dto.response.CreateMemberResponse;
+import com.example.commerceplus.domain.member.entity.Member;
+import com.example.commerceplus.domain.member.repository.MemberRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+@Transactional
+public class MemberService {
+
+    private final PasswordEncoder passwordEncoder;
+    private final MemberRepository memberRepository;
+
+    public CreateMemberResponse createMember(CreateMemberRequest request) {
+
+        // 생성패스워드와 검증패스워드가 다름
+        if (!request.isPasswordCorrect()) {
+            throw new BusinessException(ErrorCode.PASSWORD_CONFIRMATION_MISMATCH);
+        }
+
+        // 이미 존재하는 이메일
+        boolean findMember = memberRepository.existsByEmail(request.email());
+        if (findMember) {
+            throw new BusinessException(ErrorCode.DUPLICATE_EMAIL);
+        }
+
+        String encodedPassword = passwordEncoder.encode(request.password());
+
+        Member member = Member.createNormalMember(request.email(),encodedPassword, request.name(), request.phoneNumber());
+        Member savedMember = memberRepository.save(member);
+
+        return CreateMemberResponse.from(savedMember);
+    }
+
+}
