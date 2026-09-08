7929018313bc77aa534c710f015ea79b042576d1
wonyeolShin <lwy171400@gmail.com>
2026-09-08 14:51:55 +0900

    [fix/member] 관리자 로그인 관련 컨트롤러 메서드 추가

--- src/main/java/com/example/commerceplus/domain/member/controller/MemberController.java
+++ src/main/java/com/example/commerceplus/domain/member/controller/MemberController.java
@@ -3,8 +3,10 @@ package com.example.commerceplus.domain.member.controller;
 import com.example.commerceplus.common.api.ApiResponse;
 import com.example.commerceplus.domain.member.dto.request.CreateAdminRequest;
 import com.example.commerceplus.domain.member.dto.request.CreateMemberRequest;
+import com.example.commerceplus.domain.member.dto.request.LoginMemberRequest;
 import com.example.commerceplus.domain.member.dto.response.CreateAdminResponse;
 import com.example.commerceplus.domain.member.dto.response.CreateMemberResponse;
+import com.example.commerceplus.domain.member.dto.response.LoginMemberResponse;
 import com.example.commerceplus.domain.member.sevice.MemberService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
@@ -22,13 +24,18 @@ public class MemberController {
     private final MemberService memberService;
 
     @PostMapping("/signup")
-    public ResponseEntity<ApiResponse<CreateMemberResponse>> CreateMember(@Valid @RequestBody CreateMemberRequest request) {
+    public ResponseEntity<ApiResponse<CreateMemberResponse>> createMember(@Valid @RequestBody CreateMemberRequest request) {
         return ResponseEntity.ok(ApiResponse.ok(memberService.createMember(request)));
     }
 
     @PostMapping("/admins/signup")
-    public ResponseEntity<ApiResponse<CreateAdminResponse>> CreateAdmin(@Valid @RequestBody CreateAdminRequest request) {
+    public ResponseEntity<ApiResponse<CreateAdminResponse>> createAdmin(@Valid @RequestBody CreateAdminRequest request) {
         return ResponseEntity.ok(ApiResponse.ok(memberService.createAdmin(request)));
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<ApiResponse<LoginMemberResponse>> loginMember(@Valid @RequestBody LoginMemberRequest request) {
+        return ResponseEntity.ok(ApiResponse.ok(memberService.loginMember(request)));
     }
 
 }
