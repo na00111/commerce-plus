23aa9298537a011a88110aa19ea9c561820f9dcd
yun-ji98 <lyj77731@naver.com>
2026-09-09 19:13:58 +0900

    [fix/order] controller 수정

--- src/main/java/com/example/commerceplus/domain/order/controller/OrderController.java
+++ src/main/java/com/example/commerceplus/domain/order/controller/OrderController.java
@@ -1,15 +1,17 @@
 package com.example.commerceplus.domain.order.controller;
 
+import com.example.commerceplus.common.annotation.Auth;
 import com.example.commerceplus.common.api.ApiResponse;
+import com.example.commerceplus.common.jwt.JwtUser;
 import com.example.commerceplus.domain.order.dto.request.CreateOrderRequest;
 import com.example.commerceplus.domain.order.dto.response.CreateOrderResponse;
 import com.example.commerceplus.domain.order.dto.response.GetCheckoutResponse;
 import com.example.commerceplus.domain.order.service.OrderFacade;
 import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotEmpty;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -24,9 +26,10 @@ public class OrderController {
     // 주문서 미리보기
     @GetMapping("/checkout")
     public ResponseEntity<ApiResponse<GetCheckoutResponse>> getCheckoutOne(
-            @AuthenticationPrincipal Long memberId,
-            @RequestParam(required = false) List<Long> cartItemIds
+            @Auth JwtUser jwtUser,
+            @RequestParam(required = true) @NotEmpty List<Long> cartItemIds
     ) {
+        Long memberId = jwtUser.id();
         return ResponseEntity.ok(ApiResponse.ok(
                 orderFacade.getCheckoutOne(memberId, cartItemIds == null ? List.of() : cartItemIds)
         ));
@@ -35,9 +38,10 @@ public class OrderController {
     // 주문 생성
     @PostMapping
     public ResponseEntity<ApiResponse<CreateOrderResponse>> createOrder(
-            @AuthenticationPrincipal Long memberId,
+            @Auth JwtUser jwtUser,
             @Valid @RequestBody CreateOrderRequest request
     ) {
+        Long memberId = jwtUser.id();
         return ResponseEntity.status(HttpStatus.CREATED)
                 .body(ApiResponse.ok(orderFacade.createOrder(memberId, request)));
     }
