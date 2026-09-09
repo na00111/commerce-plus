2ba6051396261e4bce6b4f3a4213a231840a17f1
na00111 <angela715@naver.com>
2026-09-09 11:25:07 +0900

    feat: 회원 장바구니 조회 및 생성 기능 추가

---
+++ src/main/java/com/example/commerceplus/domain/cart/facade/CartFacade.java
@@ -0,0 +1,23 @@
+package com.example.commerceplus.domain.cart.facade;
+
+import com.example.commerceplus.domain.cart.entity.Cart;
+import com.example.commerceplus.domain.cart.repository.CartItemRepository;
+import com.example.commerceplus.domain.cart.repository.CartRepository;
+import com.example.commerceplus.domain.cart.service.CartService;
+import com.example.commerceplus.domain.member.entity.Member;
+import com.example.commerceplus.domain.member.sevice.MemberService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+
+public class CartFacade {
+
+    private final CartRepository cartRepository;
+
+    public Cart findOrCreateCart(Member member) {
+        return cartRepository.findByMemberId(member.getId())
+                .orElseGet(()-> cartRepository.save(Cart.create(member)));
+    }
+}
