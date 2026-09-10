da1a6669af96254894bebf235782ab13f86acc65
wonyeolShin <lwy171400@gmail.com>
2026-09-10 12:00:37 +0900

    [refac/product] 조건 검색을 위한 dto 생성

--- src/main/java/com/example/commerceplus/domain/product/dto/condition/SearchProductConditionRequest.java
+++ src/main/java/com/example/commerceplus/domain/product/dto/condition/SearchProductConditionRequest.java
@@ -1,10 +1,9 @@
 package com.example.commerceplus.domain.product.dto.condition;
 
 import com.example.commerceplus.domain.product.entity.ProductCategory;
-import jakarta.validation.constraints.Max;
 import jakarta.validation.constraints.Min;
 
-public record SearchProductCondition(
+public record SearchProductConditionRequest(
         @Min(0)
         Integer page,
         @Min(1)
@@ -22,7 +21,7 @@ public record SearchProductCondition(
         return minPrice > maxPrice;
     }
 
-    public SearchProductCondition {
+    public SearchProductConditionRequest {
         if (page == null) page = 0;
         if (size == null) size = 10;
     }
