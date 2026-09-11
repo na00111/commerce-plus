9000691589166a86a92fa3661249a53f147bb8a9
wonyeolShin <lwy171400@gmail.com>
2026-09-11 12:33:41 +0900

    [fix/common] cors 설정 추가

--- src/main/java/com/example/commerceplus/common/config/WebConfig.java
+++ src/main/java/com/example/commerceplus/common/config/WebConfig.java
@@ -4,6 +4,7 @@ import com.example.commerceplus.common.annotation.JwtUserArgumentResolver;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import java.util.List;
@@ -18,4 +19,15 @@ public class WebConfig implements WebMvcConfigurer {
     public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
         resolvers.add(jwtUserArgumentResolver);
     }
+
+    // cors 설정
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:5173")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedHeaders("*")
+                .allowCredentials(true);
+    }
+
 }
