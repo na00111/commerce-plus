59bf4b8564b49263f87c3d416ff9b211b5dc34ba
wonyeolShin <lwy171400@gmail.com>
2026-09-11 14:16:39 +0900

    [refac/order]  주문취소 dto 리팩토링

--- src/main/java/com/example/commerceplus/domain/order/dto/request/CancelOrderRequest.java
+++ src/main/java/com/example/commerceplus/domain/order/dto/request/CancelOrderRequest.java
@@ -1,10 +1,4 @@
 package com.example.commerceplus.domain.order.dto.request;
 
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-
-@Getter
-@NoArgsConstructor
-public class CancelOrderRequest {
-    private String action;
+public record CancelOrderRequest(String action) {
 }
