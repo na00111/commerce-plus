04d3362dfd828ee078c8afafb84967812db3a6c3
wonyeolShin <lwy171400@gmail.com>
2026-09-11 14:16:58 +0900

    [feat/payment]  주문 메서드 임시 추가

--- src/main/java/com/example/commerceplus/domain/payment/service/PaymentService.java
+++ src/main/java/com/example/commerceplus/domain/payment/service/PaymentService.java
@@ -4,9 +4,15 @@ import com.example.commerceplus.domain.order.entity.Order;
 import com.example.commerceplus.domain.payment.entity.Payment;
 import org.springframework.stereotype.Service;
 
+import java.util.Optional;
+
 @Service
 public class PaymentService {
     public static Payment createPayment(Order order, int totalPrice) {
+        return null;
+    }
+
+    public Optional<Payment> findPaymentByOrderId(Long id) {
         return null;
     }
 }
