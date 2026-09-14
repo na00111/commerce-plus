775096eef8a4832ce7785608ad0e97ad7f2cd06a
yun-ji98 <lyj77731@naver.com>
2026-09-14 14:11:56 +0900

    [fix/order] GetOrderResponse 수정

--- src/main/java/com/example/commerceplus/domain/order/dto/response/GetOrderResponse.java
+++ src/main/java/com/example/commerceplus/domain/order/dto/response/GetOrderResponse.java
@@ -9,20 +9,29 @@ public record GetOrderResponse(
         Long orderId,
         String orderNumber,
         Long paymentId,
+        String paymentStatus,
         int totalPrice,
         String status,
         String orderName,
         LocalDateTime createdAt,
         List<GetOrderItemResponse> orderItems
 ) {
-    public static GetOrderResponse from(Order order, Long paymentId) {
-        List<GetOrderItemResponse> orderItems = order.getOrderItems().stream()
+
+    public static GetOrderResponse from(
+            Order order,
+            Long paymentId,
+            String paymentStatus
+    ) {
+        List<GetOrderItemResponse> orderItems = order.getOrderItems()
+                .stream()
                 .map(GetOrderItemResponse::from)
                 .toList();
+
         return new GetOrderResponse(
                 order.getId(),
                 order.getOrderNumber(),
                 paymentId,
+                paymentStatus,
                 order.getTotalPrice(),
                 order.getStatus().name(),
                 order.getOrderName(),
