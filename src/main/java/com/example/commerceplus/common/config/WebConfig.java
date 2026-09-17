b2d5cc3dcc6b65e3390ba6bbe9c5e77ac76c2fdd
wonyeolShin <lwy171400@gmail.com>
2026-09-17 10:19:29 +0900

    [fix/common] cors 및 정적파일 설정

--- src/main/java/com/example/commerceplus/common/config/WebConfig.java
+++ src/main/java/com/example/commerceplus/common/config/WebConfig.java
@@ -5,6 +5,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import java.util.List;
@@ -20,14 +21,5 @@ public class WebConfig implements WebMvcConfigurer {
         resolvers.add(jwtUserArgumentResolver);
     }
 
-    // cors 설정
-//    @Override
-//    public void addCorsMappings(CorsRegistry registry) {
-//        registry.addMapping("/**")
-//                .allowedOrigins("http://localhost:5173")
-//                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
-//                .allowedHeaders("*")
-//                .allowCredentials(true);
-//    }
 
 }
